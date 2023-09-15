3625478485515938596ddb0007c64165a97873b8
tamta123 <tamtag186@gmail.com>
2023-09-15 19:42:51 +0400

    server updates

---
+++ config/database.js
@@ -0,0 +1,15 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+});
+
+export default pool;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,15 @@
 import express from "express";
+import pool from "../config/database.js";
 
 const app = express();
+
+app.get("/", async (_, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM galleria");
+    return res.status(200).json(result.rows);
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+});
+
+app.listen(process.env.PORT || 3000);
