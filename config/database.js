469affe0c4d71d305afbadfecb65059317f8bf32
tamta123 <tamtag186@gmail.com>
2023-09-17 17:58:20 +0400

    insert the data from data.json into sql table

--- config/database.js
+++ config/database.js
@@ -1,8 +1,12 @@
 import pkg from "pg";
 import dotenv from "dotenv";
+import fs from "fs";
 dotenv.config();
 
 const { Pool } = pkg;
+
+const jsonData = fs.readFileSync("data.json", "utf8");
+const data = JSON.parse(jsonData);
 
 const pool = new Pool({
   host: process.env.PGHOST,
@@ -11,5 +15,42 @@ const pool = new Pool({
   port: process.env.PGPORT,
   database: process.env.PGDATABASE,
 });
+
+const insertRecord = async (record) => {
+  try {
+    const query = `INSERT INTO galleria (name, year, description, source, artist_photo, artist_name, thumbnail, hero_s, hero_l, gallery)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`;
+
+    const values = [
+      record.name,
+      record.year,
+      record.description,
+      record.source,
+      record.artist.photo,
+      record.artist.name,
+      record.images.thumbnail,
+      record.images.hero.small,
+      record.images.hero.large,
+      record.images.gallery,
+    ];
+    await pool.query(query, values);
+    console.log(`Inserted record: ${record.name}`);
+  } catch (error) {
+    console.error(`Error inserting record: ${record.name}`, error);
+  }
+};
+
+// Loop through the data and insert records
+const insertDataIntoDB = async () => {
+  for (const record of data) {
+    await insertRecord(record);
+  }
+
+  // Close the database connection when done
+  pool.end();
+};
+
+// Call the function to start the data insertion process
+insertDataIntoDB();
 
 export default pool;
--- src/server.js
+++ src/server.js
@@ -8,7 +8,9 @@ app.use("/images", express.static("public/images"));
 
 app.get("/", async (_, res) => {
   try {
+    console.log("Tamta");
     const result = await pool.query("SELECT * FROM galleria");
+    console.log("tamta");
     return res.status(200).json(result.rows);
   } catch (error) {
     return res.status(500).json({ message: error });
