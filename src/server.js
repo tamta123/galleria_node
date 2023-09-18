6a1560040c3973a0efe95a9a5ffceb031d140fc9
tamta123 <tamtag186@gmail.com>
2023-09-18 20:05:44 +0400

    fix:add the missinf photo

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import pool from "../config/database.js";
 import bodyParser from "body-parser";
-import cors from "cors"; //
+import cors from "cors";
 
 const app = express();
 app.use(cors());
