2dcae5234d80e78f95a7064e0bf55ee3111a66c2
tamta123 <tamtag186@gmail.com>
2023-09-25 15:27:12 +0400

    add the new route

--- src/server.js
+++ src/server.js
@@ -8,11 +8,24 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/images", express.static("public/images"));
 
+app.get("/artist/:artistId", async (req, res) => {
+  try {
+    const { artistId } = req.params;
+    const artistQuery = `SELECT * FROM galleria WHERE id = $1`;
+    const result = await pool.query(artistQuery, [artistId]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "Artist not found" });
+    }
+    return res.status(200).json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 app.get("/", async (_, res) => {
   try {
-    console.log("Tamta");
     const result = await pool.query("SELECT * FROM galleria");
-    console.log("tamta");
     return res.status(200).json(result.rows);
   } catch (error) {
     return res.status(500).json({ message: error });
